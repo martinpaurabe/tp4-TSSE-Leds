5cbfc6b19fc0d020fbe6a9c4986006751ad1a939
Martin Paura Bersan <martin.paura@adcorpus.net>
2025-10-30 15:28:01 -0300

    Con todos los test ok

--- test/test_leds.c
+++ test/test_leds.c
@@ -180,7 +180,7 @@ void test_revisar_leds_correctamente_mapeado_en_memoria(void) {
     int i = 1;
     for (; i <= CANT_LEDS; i++) {
         led_turn_on(i);
-        TEST_ASSERT_EQUAL_HEX16(FIRST_BIT << (i - LED_OFFSET - 1),
+        TEST_ASSERT_EQUAL_HEX16(FIRST_BIT << (i - LED_OFFSET),
                                 puerto_virtual); // Corrijo el -1
         led_turn_off(i);
     }
